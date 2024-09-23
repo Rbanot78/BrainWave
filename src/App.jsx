b6a16bbd6bb8ab043ab72ce1b47cca98c08d744f
Rbanot78 <RBANOT78@GMAIL.COM>
2024-09-23 23:40:21 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import ButtonGradient from './assets/svg/ButtonGradient';
+// import Button from './Components/Button';
+import Header from './Components/Header';
+import Hero from './Components/Hero';
+import Benefits from './Components/Benefits';
+import Collaboration from './Components/Collaboration';
+import Services from './Components/Services';
+import Pricing from './Components/Pricing';
+import Roadmap from "./Components/Roadmap";
+import Footer from "./Components/Footer";
+
+const App = () => {
+  return (
+   <>
+   
+     <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hiddren'>
+     <Header/>
+     <Hero/>
+     <Benefits />
+     <Collaboration/>
+     <Services/>
+     <Pricing />
+     <Roadmap />
+     <Footer />
+     </div>
+     <ButtonGradient />
+    
+   </>
+  )
+}
+
+export default App
